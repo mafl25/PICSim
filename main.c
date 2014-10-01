9c9713122333eccf01b7703624c6ad810263df3f
mfernandez <mafernandez4j@gmail.com>
2014-09-30 19:51:08 -0600

    Se cambio dir .ui

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	gtk_init(&argc, &argv);
 
 	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder, "treeview4.ui", NULL);
+	gtk_builder_add_from_file(builder, "project_gui.ui", NULL);
 
 	window = gtk_builder_get_object(builder, "window1");
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
